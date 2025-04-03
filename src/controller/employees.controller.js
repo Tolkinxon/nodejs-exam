b3a1d26c188d16347cd7861b94173e9c37a5754b
Soliyev Tolkinxon <krosstony639@gmail.com>
2025-04-03 19:17:35 +0500

    Finish takein employee informations

--- src/controller/employees.controller.js
+++ src/controller/employees.controller.js
@@ -1,8 +1,16 @@
+import { checkToken } from "../models/checkToken.js";
 import { readFileDb } from "../models/readFile.js"
 
 export const employeesController = {
     GET: async function (req, res){
-        const employees = await readFileDb('employees');
-        res.json(employees);       
+        const token = req.headers.authorization;
+        if(token) {
+            const employee = await checkToken(req, res);
+            return res.status(200).json(employee);      
+        } 
+        if(token == undefined) {
+            const employees = await readFileDb('employees');
+            return res.status(200).json(employees);  
+        }
     }
 }
