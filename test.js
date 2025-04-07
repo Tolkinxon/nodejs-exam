e09b3fe1d42ab86215327583f77f6b2a2f80ccd6
Soliyev Tolkinxon <krosstony639@gmail.com>
2025-04-07 23:34:48 +0500

    Integrate js file

--- test.js
+++ test.js
@@ -1,13 +1,22 @@
 import fs from "node:fs";
 import { sendFile } from "./src/middlewares/sendFile.js";
 
-const arr = fs.readdirSync('./src/views');
+        
+const arrHtml= fs.readdirSync('./src/views');
+const arrAllFIles = fs.readdirSync('./public/js');
+console.log(arrAllFIles);
 
 function makeRoutes(app){
-    for(let route of arr){
+    for(let route of arrHtml){
         const r = '/'+ route;
         app.get(r,(req, res)=>{
             sendFile(route, res, false);
+        })
+    }
+    for(let route of arrAllFIles){
+        const r = '/'+ route;
+        app.get(r,(req, res)=>{
+            sendFile("/js/"+route, res, true);
         })
     }
 };
